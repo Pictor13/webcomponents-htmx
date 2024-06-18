31d0f7b7022b75f0c40ac3a1bb34f147549517f1
Igor Pellegrini <igor.pellegrini@live.com>
2024-06-18 17:23:31 +0200

    BACKUP: wip/broken state

--- backend/components/get-emoji.js
+++ backend/components/get-emoji.js
@@ -1,42 +1,68 @@
-const randomString = () => {
-  return Math.floor(Math.random() * Date.now()).toString(36);
-};
+const defaultEmoji = '🙂'
+
+const template = document.querySelector('#emoji-lottery')
+if (!template)
+  throw new Error('Unable to load emoji-lottery template')
+
+const tpl = (s, emoji) =>
+  `Here an emoji for you ${emoji ?? defaultEmoji}`
+
 
 class EmojiLotteryElement extends HTMLButtonElement {
-  // emoji;
 
   constructor() {
     super();
+  }
 
-    this.tpl = (s, emoji) => `Here an emoji for you <span>${emoji}</span>`
-    this.innerHTML = this.tpl`${this.emoji}`;
+  tpl(emoji) {
+    template?.content.cloneNode(true)
+    const emojiContainer = template?.querySelector('span')
+    if (!emojiContainer)
+      throw new Error('Cannot find emojiContainer inside template')
+    emojiContainer.innerText = emoji
+
+    return template?.innerHTML.replace('{{emoji}}', emoji)
   }
 
   connectedCallback() {
-    this.innerHTML = this.tpl`${this.emoji}`
-
-    // emoji container
-    this.emoji = this.querySelector('span')
-    if (!this.emoji)
-      throw new Error('Template contains no "span"')
-    this.emoji.id = `emoji-lottery_container-${randomString()}`
+    this.innerHTML = tpl(defaultEmoji)
 
     // swap emoji on button.click (request emoji to backend)
     this.setAttribute('hx-get','/emoji/lottery')
     this.setAttribute('hx-swap', 'innerHTML')
-    this.setAttribute('hx-target', '#'+ this.emoji.id)
+    // this.setAttribute('hx-trigger', 'process-template')
+    // this.setAttribute('mustache-array-template', 'emoji-lottery')
+
+    document.addEventListener('htmx:beforeSwap', event => {
+      const content = event.detail.target.outerHTML; // Get the fetched content
+      // Replace the target element content with the processed content
+      event.detail.target.outerHTML = tpl(content);
+
+      console.log('PROCESSED');
+    });
+    document.addEventListener('htmx:afterRequest', function(evt) {
+      if(evt.detail.xhr.status == 404){
+          /* Notify the user of a 404 Not Found response */
+          return alert("Error: Could Not Find Resource");
+      } 
+      if (evt.detail.successful != true) {
+          /* Notify of an unexpected error, & print error to console */
+          alert("Unexpected Error");
+          return console.error(evt);
+      }
+      if (evt.detail.target.id == 'info-div') {
+          /* Execute code on the target of the HTMX request, which will
+          be either the hx-target attribute if set, or the triggering 
+          element itself if not set. */
+          let infoDiv = document.getElementById('info-div');
+          infoDiv.style.backgroundColor = '#000000';  // black background
+          infoDiv.style.color = '#FFFFFF';  // white text
+      }
+  });
   }
 
   // disconnectedCallback() {
   //   this.removeEventListener("click", this.randomEmoji);
-  // }
-
-  // async randomEmoji() {
-  //   fetch('/emoji/lottery')
-  //     .then(response => response.text())
-  //     .then(data => this.innerHTML = this.tpl`${data}`)
-  //     .catch(error => console.error('Error:', error)
-  //   );
   // }
 }
 
